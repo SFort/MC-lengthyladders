2fa7935a3e7e77794b4bea5bf27ff309117d11de
SFort <github@ssf.tf>
2020-08-21 15:58:40 +0200

    init

---
+++ src/main/java/sf/ssf/sfort/mixin/LongLadder.java
@@ -0,0 +1,53 @@
+package sf.ssf.sfort.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.RayTraceContext;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LadderBlock.class)
+public abstract class LongLadder {
+	//TODO this level of 'if' makes me kinda sick
+ 	@Inject(method = "canPlaceAt", at = @At("HEAD"),cancellable = true)
+	public void canPlaceAt(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable info) {
+		BlockState down = world.getBlockState(pos.down());
+		if (down.isSideSolidFullSquare(world,pos.up(),Direction.UP)){
+			info.setReturnValue(true);
+			info.cancel();
+		}
+		Direction laddir = null;
+		if (SideShapeType.FULL.matches(state, world, pos, Direction.EAST))
+			laddir=Direction.EAST;
+		if (SideShapeType.FULL.matches(state, world, pos, Direction.SOUTH))
+			laddir=Direction.SOUTH;
+		if (SideShapeType.FULL.matches(state, world, pos, Direction.WEST))
+			laddir=Direction.WEST;
+		if (SideShapeType.FULL.matches(state, world, pos, Direction.NORTH))
+			laddir=Direction.NORTH;
+		if (laddir !=null){
+			if(SideShapeType.FULL.matches(world.getBlockState(pos.down()), world, pos.down(), laddir)) {
+				info.setReturnValue(true);
+				info.cancel();
+			}else if(world.getBlockState(pos.offset(laddir)).isOf(Blocks.LADDER)){
+				info.setReturnValue(false);
+				info.cancel();
+			}
+		}
+	}
+	@Inject(method = "getStateForNeighborUpdate", at = @At("HEAD"),cancellable = true)
+	public void getStateForNeighborUpdate(BlockState state, Direction direction, BlockState newState, WorldAccess world, BlockPos pos, BlockPos posFrom,CallbackInfoReturnable<BlockState> info) {
+		if (!state.canPlaceAt(world, pos)){
+			info.setReturnValue(Blocks.AIR.getDefaultState());
+			info.cancel();
+		}
+	}
+}
