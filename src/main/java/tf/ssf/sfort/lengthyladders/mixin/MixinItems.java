a6b156b862e4c45a41233c00967b453e95dd0889
SFort <github@ssf.tf>
2021-06-10 09:05:01 +0200

    Fix #5

--- src/main/java/tf/ssf/sfort/lengthyladders/mixin/MixinItems.java
+++ src/main/java/tf/ssf/sfort/lengthyladders/mixin/MixinItems.java
@@ -1,17 +1,28 @@
 package tf.ssf.sfort.lengthyladders.mixin;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import tf.ssf.sfort.lengthyladders.LadderBlockItem;
 
 @Mixin(Items.class)
 public abstract class MixinItems {
-	//public static Item LADDER = Registry.register(Registry.ITEM, Registry.BLOCK.getId(Blocks.LADDER), new LadderBlockItem(Blocks.LADDER, (new Item.Settings()).group(ItemGroup.DECORATIONS)));
+	@Inject(method = "register(Lnet/minecraft/block/Block;Lnet/minecraft/item/ItemGroup;)Lnet/minecraft/item/Item;", at=@At("HEAD"), cancellable = true)
+	private static void register(Block block, ItemGroup group, CallbackInfoReturnable<Item> cir){
+		if(block.equals(Blocks.LADDER))
+			cir.setReturnValue(register(new LadderBlockItem(block, (new Item.Settings()).group(group))));
+	}
 	@Shadow
-	public static Item LADDER = Registry.register(Registry.ITEM, 186, Registry.BLOCK.getId(Blocks.LADDER).toString(), new LadderBlockItem(Blocks.LADDER, new Item.Settings().group(ItemGroup.DECORATIONS)));
+	private static Item register(BlockItem blockItem) { return null; }
 }
